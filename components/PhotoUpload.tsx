7ff2150b98e56e802200d57f261802c5fddc3acc
Norkiee <adinorkielawerteh@gmail.com>
2026-01-28 11:15:46 +0000

    Fix multiple photo upload by resetting input after selection

--- components/PhotoUpload.tsx
+++ components/PhotoUpload.tsx
@@ -74,7 +74,10 @@ export default function PhotoUpload({ photos, onPhotosChange, maxPhotos = 10 }: 
               accept="image/*"
               multiple
               className="hidden"
-              onChange={(e) => handleFileChange(e.target.files)}
+              onChange={(e) => {
+                handleFileChange(e.target.files)
+                e.target.value = '' // Reset input to allow selecting more files
+              }}
             />
             <svg className="w-8 h-8 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
